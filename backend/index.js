9024368fc96c0104e0a93ee7112359f35a5c229f
sunnrooy-stack <sunnrooy@gmail.com>
2026-05-11 11:49:42 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-   origin:"http://localhost:5173",
+   origin:"https://playtube-c8zb.onrender.com",
    credentials:true
 }))
 
